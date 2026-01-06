58ac3511521577482f117e98177439986bcc2f3a
Harman-samra <146431176+Harman-samra@users.noreply.github.com>
2026-01-06 21:43:18 +0000

    hi

--- sample_app.py
+++ sample_app.py
@@ -4,7 +4,7 @@ sample = Flask(__name__)
 
 @sample.route("/")
 def main():
-    return render_template("index.html")
+    return render_template("Urban_Eats_Home_Page.html")
 
 if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=5000)
