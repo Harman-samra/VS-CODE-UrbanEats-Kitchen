c1256a6d2b25f03ee2493349e308a46e857497e0
Harman-samra <146431176+Harman-samra@users.noreply.github.com>
2026-01-18 20:19:26 +0000

    buttons

--- sample_app.py
+++ sample_app.py
@@ -6,9 +6,17 @@ sample = Flask(__name__)
 def main():
     return render_template("Urban_Eats_Home_Page.html")
 
+@sample.route("/menu")
+def menu():
+    return render_template("UrbanEats Menu page.html")
+
 @sample.route("/about")
 def about():
     return render_template("about_us.html")
+
+@sample.route("/book")
+def book():
+    return render_template("Urban_Eats_Home_Page.html")
 
 if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=5000)
